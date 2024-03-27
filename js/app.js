9938bd4beafc8d16a940ae746d5ac6d4ebfcc514
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-27 17:25:00 +0100

    ADD : commit

--- js/app.js
+++ js/app.js
@@ -15,6 +15,11 @@
 // Visualizzare a fianco ad ogni item ha una “x”:
 // cliccando su di essa, il todo viene rimosso dalla lista.
 
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante “aggiungi”:
+// cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
+// che quindi viene aggiunto alla lista dei todo esistenti.
+
 
 
 const { createApp } = Vue
@@ -22,6 +27,10 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
+        actionAdd: {
+            text : '',
+            done : false,
+        },
         textDecoration: 'text-overline',
         message: 'VUE TODOLIST',
         actions: [
@@ -60,6 +69,18 @@ const { createApp } = Vue
         remove(i){
             console.log(i)
             this.actions.splice(i, 1)
+        },
+        
+        addTodo(){
+             this.actions.push(this.actionAdd)
+            
+        },
+        resetInput(string){
+            if(string !== ''){
+                string = '';
+
+                return string
+            }
         }
     }
   }).mount('#app')
