7ecbd4136347b156066c747b9dd121307fa931cc
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-27 15:13:10 +0100

    ADD: start file

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// text, una stringa che indica il testo del todo
+// done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// NON perdetevi a fare troppi abbellimenti con il css all’inizio, ne a gestire layout particolari.
+// Abbellirete tutto alla fine.
+// Scrivete solo il CSS necessario a completare le milestone
+
+
+// MILESTONE 1
+// Stampare all’interno di una lista HTML un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'VUE TODOLIST',
+        actions: [
+            {
+                text: 'Comparare arance',
+                done: false,
+            },
+            {
+                text: 'Comparare cibo cani',
+                done: false,
+            },
+            {
+                text: 'Andare dal commercialista',
+                done: true,
+            },
+            {
+                text: 'Studiare Vue',
+                done: true,
+            },
+            {
+                text: 'Andare a caricare furgone',
+                done: false,
+            },
+            {
+                text: 'Fare trasloco dalla nonna',
+                done: true,
+            },
+            {
+                text: 'Fare esercizio del pomeriggio',
+                done: false,
+            },
+        ]
+      }
+    }
+  }).mount('#app')
